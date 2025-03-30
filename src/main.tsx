bcac94d35619153b1ddd51e71c942e4fa8af5812
Kokonutman <arjun.iyer42@gmail.com>
2025-03-30 08:26:12 -0400

    added env vars

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,17 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { Auth0Provider } from '@auth0/auth0-react';
-import { RoleProvider } from './contexts/RoleContext';
-import App from './App.tsx';
-import './index.css';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { Auth0Provider } from "@auth0/auth0-react";
+import { RoleProvider } from "./contexts/RoleContext";
+import App from "./App.tsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Auth0Provider
-      domain="YOUR_AUTH0_DOMAIN"
-      clientId="YOUR_AUTH0_CLIENT_ID"
+      domain="dev-e4ku1v7ew7mf7e2u.us.auth0.com"
+      clientId="1zcMLi8sGrpqg73VnXsZnJ4lbcBWQBQV"
       authorizationParams={{
-        redirect_uri: window.location.origin
+        redirect_uri: window.location.origin,
       }}
     >
       <RoleProvider>
